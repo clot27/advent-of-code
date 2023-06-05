b1cbc692e4f71e1ae7e8d2fa6ee5f77fcfd688f5
Aditya <clot27@apx_managed.vanilla>
2023-06-05 15:37:00 +0530

    day1

---
+++ 2023/day1/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+fn main() {
+    let mut k = extract_input_to_vec();
+    let max_value = &k.iter().max();
+    match max_value {
+        // For first part of puzzle
+        Some(max) => println!("Max value: {}", max),
+        None => println!("Vector is empty"),
+    }
+    k.sort_by(|a, b| b.cmp(a)); // For second part of puzzle
+    let top_three: i32 = k.iter().take(3).sum();
+    println!("Top three: {}", top_three);
+}
+
+fn extract_input_to_vec() -> Vec<i32> {
+    let contents =
+        fs::read_to_string("demo_input.txt").expect("Should have been able to read the file");
+    let mut elf: Vec<i32> = Vec::new();
+    let mut initial: i32 = 0;
+    for line in contents.split('\n') {
+        if !line.is_empty() {
+            initial += line.parse::<i32>().unwrap();
+        } else {
+            elf.push(initial);
+            initial = 0;
+            continue;
+        }
+    }
+    elf
+}
